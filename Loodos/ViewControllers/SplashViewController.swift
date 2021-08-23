7a775b50b41c86e66c6d47bd9e5ec25df9b701df
Baran Gungor <baran.gngr@gmail.com>
2021-08-23 13:12:19 +0300

    Add project and folder design

---
+++ Loodos/ViewControllers/SplashViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Loodos
+//
+//  Created by Baran on 23.08.2021.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+  
+  // MARK: Properties
+  @IBOutlet weak var splashLabel: UILabel!
+  
+  // MARK: LifeCycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
+
---
+++ UIColor+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+Extensions.swift
+//  Loodos
+//
+//  Created by Baran on 23.08.2021.
+//
+
+import UIKit
+
+extension UIColor {
+
+  @nonobjc class var charlestonGreen: UIColor {
+    return #colorLiteral(red: 0.1568627451, green: 0.1647058824, blue: 0.1921568627, alpha: 1)
+  }
+
+}
